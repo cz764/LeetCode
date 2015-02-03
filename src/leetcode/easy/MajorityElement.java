370c5543ec660a28cea5d4c30c4b81bb16e56868
Chen Zhu <cz764@nyu.edu>
2015-02-03 13:55:56 -0500

    majority element

--- src/leetcode/easy/MajorityElement.java
+++ src/leetcode/easy/MajorityElement.java
@@ -1,17 +1,29 @@
 package leetcode.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Jane on 2/3/15.
  */
 public class MajorityElement {
     public static void main(String[] args) {
-        int[] test = {1, 1, 1, 1, 1, 4, 4, 3};
+        int[] test = {1, 1, 1, 1, 1, 4, 4, 3, 1};
 
         System.out.println(majorityElement(test));
     }
 
     public static int majorityElement(int[] num) {
-        int result = 0;
-        return result;
+        int n = num.length;
+        Map<Integer, Integer> dic = new HashMap<Integer, Integer>();
+
+        for (int e: num) {
+            if (dic.get(e) == null) dic.put(e, 1);
+            else 
+                dic.put(e, dic.get(e) + 1);
+
+            if (dic.get(e) > n / 2) return e;
+        }
+        return 0;
     }
 }
