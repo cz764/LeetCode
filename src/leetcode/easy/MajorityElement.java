e201ad6d35eb6edd1747ae1a6867d7d4dd985b0d
Chen Zhu <cz764@nyu.edu>
2015-02-03 10:11:58 -0500

    prep MajorityElement

---
+++ src/leetcode/easy/MajorityElement.java
@@ -0,0 +1,17 @@
+package leetcode.easy;
+
+/**
+ * Created by Jane on 2/3/15.
+ */
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] test = {1, 1, 1, 1, 1, 4, 4, 3};
+
+        System.out.println(majorityElement(test));
+    }
+
+    public static int majorityElement(int[] num) {
+        int result = 0;
+        return result;
+    }
+}
